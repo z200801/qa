28e34cd4ed1ee3e41d2deefbd583a2d263a06f1b
z200801 <94222461+z200801@users.noreply.github.com>
2021-11-13 12:25:24 +0200

    Update demoqa-autotest.py

--- demoqa-autotest.py
+++ demoqa-autotest.py
@@ -1,1 +1,18 @@
+from selenium import webdriver
 
+driver = webdriver.Chrome()
+
+driver.get('https://demoqa.com/login')
+
+driver.find_element_by_xpath("//input[@id='userName']").send_keys("UserTest1")
+driver.find_element_by_xpath("//input[@id='password']").send_keys("UserTest1")
+
+driver.find_element_by_xpath("//buttun[@id='login']").click()
+
+userNameValue=driver.find_element_by_xpath("//label[@id='userNmae-value']").text
+
+print(userNameValue)
+
+assert "UserTest1" in userNameValue
+
+driver.close()
