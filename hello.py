e3a35dc9ba6ba5d6902525b3361c8f6ba50f60b1
z200801@gmail.com <z200801@gmail.com>
2022-01-08 23:41:33 +0200

    m2

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-msg = "Hello wrld"
+msg = "Hello wrld 3423"
 msg1=msg.split()
+msg2=msg.strip(msg)
 
 def prt_info(msg_str,msg_print):
     print(msg_str,"=",msg_print)
@@ -11,3 +12,6 @@ print(chr(27) + "[2J")
 prt_info("msg", msg)
 prt_info("msg1[0]", msg1[0])
 prt_info("msg1[1]", msg1[1])
+prt_info("msg1[1]", msg1[1])
+
+print("msg2=",msg2)
